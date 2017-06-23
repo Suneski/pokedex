403d4320b4e44b554de26f5abaa32dc0c1abac91
David Espinoza <despinoza1027@gmail.com>
2017-06-23 15:08:10 -0500

    initial ajax call working

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -1,15 +1,39 @@
 import React, { Component } from 'react';
+import $ from 'jquery';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      searchTerm: '',
+      results: []
+    }
+  }
+
+  updateSearchTerm(evt) {
+    this.setState({
+      searchTerm: evt.target.value
+    })
+  }
+
+  makeAjaxCall() {
+    $.ajax({
+      url: 'http://pokeapi.co/api/v2/pokemon/' + this.state.searchTerm
+    })
+    .done((data) => {
+      this.setState({
+        searchTerm: '',
+        results: data
+      })
+    });
+  };
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <input onKeyUp={(evt) => this.updateSearchTerm(evt)}/>
+        <button onClick={() => this.makeAjaxCall()}>SEARCH</button>
       </div>
     );
   }
