955a9134026a14c2d13842d9012a75c9bc214485
David Espinoza <despinoza1027@gmail.com>
2017-06-25 18:48:48 -0500

    added missingno to error

--- react-ui/src/Error.js
+++ react-ui/src/Error.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Missingno from './images/wild-missingno.jpg';
 
 class Error extends React.Component {
   render() {
     return (
       <div className={this.props.errorVisibility}>
-        Pokemon Not Found!
+        <p className="error-text">Pokemon Not Found!</p>
+        <img src={Missingno} alt="missingno" className="missingno"/>
       </div>
     )
   }
